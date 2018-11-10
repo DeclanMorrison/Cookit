90ac0d83e64b4b8a0233019cfbd6d9ee13f1f85a
Vito Daniel <vitodaniel93@gmail.com>
2018-11-10 10:56:08 -0500

    Added Appbar with Logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,6 @@
-//All page-level components will be arranged here, and switched using the React Router.
-
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { 
-  AppBar, 
-  Button,
-  Toolbar,
-  Typography } from "@material-ui/core";
+import Appbar from "./components/Appbar";
 
-const App = () => (
-  <div>
-    <AppBar position="static" color="default">
-      <Toolbar>
-        <Typography variant="h6" color="inherit">
-          Photos
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  </div>
-);
+const App = () => <Appbar />;
 
 export default App;
