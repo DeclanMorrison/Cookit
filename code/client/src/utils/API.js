e5b7afeaa4c46e74a20cb57463ce7726eb906678
Izaak <37053755+Izaakmills@users.noreply.github.com>
2018-11-15 20:53:03 -0500

    updating utils/api to talk to backend using axios

--- code/client/src/utils/API.js
+++ code/client/src/utils/API.js
@@ -1,20 +1,24 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  // Gets all Recipes
+  getRecipes: function() {
+    return axios.get("/api/recipes");
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  // Gets the Recipes with the given id
+  getRecipe: function(id) {
+    return axios.get("/api/recipe/" + id);
   },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+  // Deletes the Recipes with the given id
+  deleteRecipe: function(id) {
+    return axios.delete("/api/recipe/" + id);
   },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  // Saves a recipe to the database
+  saveRecipe: function(bookData) {
+    return axios.post("/api/saveRecipe", bookData);
+  },
+  query: function() {
+    return axios.get("/api/search")
   }
+
 };
