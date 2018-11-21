3da0261246ee18b58246b2685ad7839ce1a982e7
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-11-21 11:37:52 -0500

    sign in working redirecting to home page, some react updates for routing

--- code/controllers/controller.js
+++ code/controllers/controller.js
@@ -1,30 +1,64 @@
-const db = require('../models')
-var exports = module.exports = {}
+const db = require("../models");
+var passport = require("../config/passport");
+var exports = (module.exports = {});
 
-exports.login = function(req,res) {
-    console.log("testcontroller hit")
-    res.render("/")
-}
-exports.signup = function(req,res) {
-    console.log("signup route hit")
-    let newuser = req.body
-    db.User.create({
-      firstName: newuser.firstName,
-      lastName: newuser.lastName,
-      email: newuser.email,
-      password: newuser.password
+exports.login = 
+    passport.authenticate("local", {
+      successRedirect: "/login",
+      failureRedirect: "/login"
     })
-      .then(function() {
-        res.redirect("/home")
-        // res.redirect("/home");
+
+exports.signup = function(req, res) {
+  console.log("signup route hit");
+  let newuser = req.body;
+  // check if email is already in database, error out if true
+  db.User.findAll({
+    where: {
+      email: newuser.email
+    }
+  }).then(function(result) {
+    if (result != "") {
+      console.log(`\n email already taken! \n`);
+      res.send({
+        status:'failed',
+        reason:"email already taken"
       })
-      .catch(function(err) {
-        console.log(err);
-        res.json(err);
-        // res.status(422).json(err.errors[0].message);
-      });
-}
+    }
+    if (result == ""){
+      db.User.create({
+        firstName: newuser.firstName,
+        lastName: newuser.lastName,
+        email: newuser.email,
+        password: newuser.password
+      })
+        .then(function() {
+          res.send({
+            status:'success',
+            reason:"user signed up"
+          });
+          // res.redirect("/home");
+        })
+        .catch(function(err) {
+          console.log(err);
+          res.json(err);
+          // res.status(422).json(err.errors[0].message);
+        });
+    }
+  });
+};
 
-exports.findRecipes = function(res,req) {
+exports.findRecipes = function(res, req) {};
 
+exports.getUserData = function(req,res) {
+  if (!req.user) {
+    // The user is not logged in, send back an empty object
+    res.json({});
+  } else {
+    // Otherwise send back the user's email and id
+    // Sending back a password, even a hashed password, isn't a good idea
+    res.json({
+      email: req.user.email,
+      id: req.user.id
+    });
+  }
 }
--- code/routes/api/index.js
+++ code/routes/api/index.js
@@ -9,68 +9,42 @@ var passport = require("../../config/passport");
 
 // module.exports = function(app) {
 
-  router.get("/testcontroller", controller.login);
-  router.get("/testsignup", controller.signup);
+router.post("/signup", controller.signup);
 
-  // // signup routing
-  router.post("/signup", controller.signup)
- 
-  // // login section
-  router.post(
-    "/login",
-    passport.authenticate("local", {
-      successRedirect: "/home",
-      failureRedirect: "/login"
-    })
-    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
-    // So we're sending the user back the route to the members page because the redirect will happen on the front end
-    // They won't get this or even be able to access this page if they aren't authed
-  );
+// // login section
+router.post("/login", controller.login);
 
-   // Route for getting some data about our user to be used client side
-   router.get("/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    }
-    else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
-    }
-  });
+// Route for getting some data about our user to be used client side
+router.get("/user_data", controller.getUserData);
 
-  // app.get("/api/recipes", (req, res) => {
-  //   console.log("\nRetreiving all recipes...\n");
-  //   db.Recipes.findAll({}).then(dbRecipes => {
-  //     res.send("it worked finally");
-  //   });
-  // });
+// app.get("/api/recipes", (req, res) => {
+//   console.log("\nRetreiving all recipes...\n");
+//   db.Recipes.findAll({}).then(dbRecipes => {
+//     res.send("it worked finally");
+//   });
+// });
 
-  // app.get("/recipe/id", function(req, res) {
-  //   console.log("\nRetreiving recipe...\n");
-  //   db.Favorites.findOne({
-  //     where: { recipeID: req.recipeID }
-  //   }).then(dbUser => {
-  //     console.log(dbUser);
-  //     res.redirect("/");
-  //   });
-  // });
+// app.get("/recipe/id", function(req, res) {
+//   console.log("\nRetreiving recipe...\n");
+//   db.Favorites.findOne({
+//     where: { recipeID: req.recipeID }
+//   }).then(dbUser => {
+//     console.log(dbUser);
+//     res.redirect("/");
+//   });
+// });
 
-  // app.get("/search", function(req, res) {
-  //   axios
-  //     .get(
-  //       "https://api.edamam.com/search?q=chicken&app_id=f457772e&app_key=47c5a1d77ba0337a17e3f917071f5c6e"
-  //     )
-  //     .then(results => {
-  //       console.log(results.data.hits[1]);
-  //     })
-  //     .catch(error => {
-  //       console.log(`Error in query: ${error}`);
-  //     });
-  // });
+// app.get("/search", function(req, res) {
+//   axios
+//     .get(
+//       "https://api.edamam.com/search?q=chicken&app_id=f457772e&app_key=47c5a1d77ba0337a17e3f917071f5c6e"
+//     )
+//     .then(results => {
+//       console.log(results.data.hits[1]);
+//     })
+//     .catch(error => {
+//       console.log(`Error in query: ${error}`);
+//     });
+// });
 // };
-module.exports = router
+module.exports = router;
