79d5b538a0124f5226db1ca32a6dc21f73efbdf7
Eshaffer <edmond.shaffer@gmail.com>
2018-12-01 10:48:15 -0500

    local storage for calendar

--- code/client/src/pages/Home.js
+++ code/client/src/pages/Home.js
@@ -6,107 +6,138 @@ import Calendar from "../components/Calendar";
 import { Typography, Divider } from "@material-ui/core";
 import 'typeface-fjalla-one';
 import SearchBar from '../components/SearchBar';
-import {Grid} from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 
 const styles = {
-    sectionHeader: {
-      fontFamily: "Fjalla One",
+  sectionHeader: {
+    fontFamily: "Fjalla One",
+  },
+  grid: {
+    overflowX: 'auto'
+  },
+  // searchBar: {
+  //   marginBottom: '100px'
+  // }
+};
+
+class Home extends React.Component {
+
+  state = {
+    calendarRecipes: {
+      "Monday": {},
+      "Tuesday": {},
+      "Wednesday": {},
+      "Thursday": {},
+      "Friday": {},
+      "Saturday": {},
+      "Sunday": {},
     },
-    grid: {
-      overflowX: 'auto'
-    },
-    // searchBar: {
-    //   marginBottom: '100px'
-    // }
+    recipes: [],
+    searchTerm: ""
   };
+
+  handleAddRecipeToCalendar = (recipe, day) => {
+
+    let newDay = this.state.calendarRecipes[day];
+    const newCalendarRecipes = this.state.calendarRecipes;
+
+    newDay = recipe;
+    newCalendarRecipes[day] = newDay;
   
-  class Home extends React.Component {
+    // setter
+
+    this.setState({ calendarRecipes: newCalendarRecipes });
+    // localStorage.setItem('recipes', JSON.stringify(day.newCalendarRecipes));
+
+    // // getter
+    // localStorage.getItem('myData');
+  };
+
+  handleUpdateSearchTerm = (term) => {
     
-    state ={ 
-      calendarRecipes: {
-        "Monday" : {},
-        "Tuesday" : {},
-        "Wednesday" : {},
-        "Thursday" : {},
-        "Friday" : {},
-        "Saturday" : {},
-        "Sunday" : {},
-      },
-      recipes : [],
-      searchTerm: ""
-    };
-
-    handleAddRecipeToCalendar = (recipe, day) => {
-      let newDay = this.state.calendarRecipes[day];
-      const newCalendarRecipes = this.state.calendarRecipes;
-
-      newDay = recipe;
-      newCalendarRecipes[day] = newDay;
-
-      this.setState({calendarRecipes: newCalendarRecipes});
-    };
-
-    handleUpdateSearchTerm = (term) => {
-      this.setState({searchTerm: term});
-    };
-
-    handleUpdateRecipes = (recipes) => {
-      // this.setState({recipes : {}});
-      this.setState({recipes : recipes.hits});
-    };
-
-    handleUpdateCalendarRecipes = (recipe, day) => {
-      const calendarRecipesState = this.state.calendarRecipes;
-      calendarRecipesState[day] = recipe;
-      this.setState({calendarRecipes: calendarRecipesState})
-    }
-
-    // componentDidMount = () => {
-    //   axios.get("https://api.edamam.com/search", {
-    //     params: {
-    //       q: "lobster",
-    //       app_id: "f457772e",
-    //       app_key: "47c5a1d77ba0337a17e3f917071f5c6e"
-    //     }
-    //   }).then(response => {
-    //     this.setState({recipes : response});
-    //   });
-    // };
-
-    render() {
-      const { classes } = this.props;
-      return (
-        <React.Fragment>
-          <Appbar calendarRecipes={this.state.calendarRecipes}>
-            <Typography className={classes.sectionHeader} font="typeface-fjalla-one" variant="h3"> This Week</Typography>
-            
-            <Divider/>
-            <br/>
-            <Calendar calendarRecipes={this.state.calendarRecipes}/>
-
-            <br/>
-            
-            <br/>
-            
-            <Grid container spacing={8} justify="space-between" alignItems="flex-end">
-              <Grid item>
-                <Typography className={classes.sectionHeader} font="typeface-fjalla-one" variant="h3">{this.state.searchTerm ? ` Recipes for ${this.state.searchTerm}` : ` Recommended Recipes`}</Typography>
-              </Grid>
-              
-              <Grid item>
-                <SearchBar handleUpdateSearchTerm={this.handleUpdateSearchTerm} handleUpdateRecipes={this.handleUpdateRecipes} className={classes.searchBar}></SearchBar>
-              </Grid>
-            </Grid>
-            
-            
-            <Divider/>
-            <br/>
-            <Recipes recipes={this.state.recipes} handleAddRecipeToCalendar={this.handleAddRecipeToCalendar}/>
-          </Appbar>
-        </React.Fragment>
-      );
-    };
-
+    this.setState({ searchTerm: term });
   };
+
+  handleUpdateRecipes = (recipes) => {
+    
+    // this.setState({recipes : {}});
+    this.setState({ recipes: recipes.hits });
+  };
+
+  handleUpdateCalendarRecipes = (recipe, day) => {
   
-  export default withStyles(styles)(Home);
+    const calendarRecipesState = this.state.calendarRecipes;
+    calendarRecipesState[day] = recipe;
+    this.setState({ calendarRecipes: calendarRecipesState })
+   
+  }
+ 
+  componentWillMount(){
+    localStorage.getItem("day") && this.setState({
+      days: JSON.parse(localStorage.getItem('day')),
+      isLoading: false
+    })
+  }
+
+  componentDidMount(){
+    if(!localStorage.getItem('day')){
+      
+    }else{
+      console.log("Using data from localStorage")
+    }
+   
+  }
+
+  componentWillUpdate(nextProps,nextState){
+    localStorage.setItem("day",JSON.stringify(nextState.calendarRecipes));
+  } 
+
+  // componentDidMount = () => {
+  //   axios.get("https://api.edamam.com/search", {
+  //     params: {
+  //       q: "lobster",
+  //       app_id: "f457772e",
+  //       app_key: "47c5a1d77ba0337a17e3f917071f5c6e"
+  //     }
+  //   }).then(response => {
+  //     this.setState({recipes : response});
+  //   });
+  // };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <React.Fragment>
+        <Appbar calendarRecipes={this.state.calendarRecipes}>
+          <Typography className={classes.sectionHeader} font="typeface-fjalla-one" variant="h3"> This Week</Typography>
+
+          <Divider />
+          <br />
+          <Calendar calendarRecipes={this.state.calendarRecipes} />
+
+          <br />
+
+          <br />
+
+          <Grid container spacing={8} justify="space-between" alignItems="flex-end">
+            <Grid item>
+              <Typography className={classes.sectionHeader} font="typeface-fjalla-one" variant="h3">{this.state.searchTerm ? ` Recipes for ${this.state.searchTerm}` : ` Recommended Recipes`}</Typography>
+            </Grid>
+
+            <Grid item>
+              <SearchBar handleUpdateSearchTerm={this.handleUpdateSearchTerm} handleUpdateRecipes={this.handleUpdateRecipes} className={classes.searchBar}></SearchBar>
+            </Grid>
+          </Grid>
+
+
+          <Divider />
+          <br />
+          <Recipes recipes={this.state.recipes} handleAddRecipeToCalendar={this.handleAddRecipeToCalendar} />
+        </Appbar>
+      </React.Fragment>
+    );
+  };
+
+};
+
+export default withStyles(styles)(Home);
